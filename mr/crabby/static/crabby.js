5bb78a54f3e26b9bfc0031819562120ffa67fbd1
David Glick <dglick@gmail.com>
2012-03-24 15:03:51 -0700

    Prepare mr.crabby 1.2.

--- mr/crabby/static/crabby.js
+++ mr/crabby/static/crabby.js
@@ -2,7 +2,7 @@ jQuery(function($) {
 
 var $crab = $('<div class="crabby"><div class="arm"><div class="claw"></div></div></div>')
 	.appendTo('body');
-$('#searchGadget').keypress(function() {
+$('#search-site,#searchGadget').keypress(function() {
   if ($(this).val() == 'cioppino') {
   	$crab.addClass('attacking');
   	$(document).bind('click', function() {
