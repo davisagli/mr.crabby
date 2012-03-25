a7229e67d2d9c49b901a549fa19904b73d15bb86
David Glick <dglick@gmail.com>
2012-03-25 00:20:39 -0700

    load the divs (and images) lazily

--- mr/crabby/static/crabby.js
+++ mr/crabby/static/crabby.js
@@ -1,8 +1,12 @@
 jQuery(function($) {
 
-var $crab = $('<div class="crabby"><div class="arm"><div class="claw"></div></div></div>')
-	.appendTo('body');
+var loaded = false;
 $('#search-site,#searchGadget').keypress(function() {
+  var $crab = $('.crabby');
+  if (!$crab.length) {
+  	$crab = $('<div class="crabby"><div class="arm"><div class="claw"></div></div></div>')
+	.appendTo('body');
+  }
   if ($(this).val() == 'cioppino') {
   	$crab.addClass('attacking');
   	$(document).bind('click', function() {
